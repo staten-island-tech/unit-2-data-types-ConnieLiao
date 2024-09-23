fd99e0f4ab5ef7ca4be9676479467e963b33ff07
ConnieLiao <conniel32@nycstudents.net>
2024-09-23 10:11:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,29 @@ y= x.split( )
 z = y[0]
 print(y)
 print(z)
-Day_of_the_week = input("What day is today")
+Day_of_the_week = input("What day is today? ")
 if Day_of_the_week == "Friday":
     print ("corect")
 else:
     print ("incorrect")
-
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+if 887 % 2 == 0:
+    print("the number is even") 
+else:
+    print("the number is odd")
+tip = service was bad
+bill = 18.90 
+if tip == service was bad and bill == 18.90:
+    print("bill+tip 0%")
+elif tip == service was okay and bill == 18.90:
+    print("bill+tip 15%")
+elif tip == service was good and bill == 18.90:
+    print("bill+tip 20%")
+else:
+    print("bill+tip 255")
